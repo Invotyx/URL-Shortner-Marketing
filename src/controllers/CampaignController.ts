52798d0ce93f4e40d023e9245c1ed9c500d53ee4
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2020-09-16 16:41:55 +0500

    Fix get-statistics api

--- src/controllers/CampaignController.ts
+++ src/controllers/CampaignController.ts
@@ -318,7 +318,7 @@ export const getStatistics = async(req: Request, res: Response) => {
       const campaigns = await getRepository(CampaignView).find({
         where:{
           campaign: campaign,
-          created_at: Between(req.body.starts_at, req.body.ends_at),
+          created_at: Between(req.query.starts_at, req.query.ends_at),
         },
       });
       return res.status(200).json({
