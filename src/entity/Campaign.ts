858703eba4761e1a2540b12c14c9ccae7604bf93
Muhammad Umer Farooq <umer.farooq@invotyx.com>
2020-11-05 17:38:44 +0500

    Fix nullable on meta variables in campaign

--- src/entity/Campaign.ts
+++ src/entity/Campaign.ts
@@ -1,81 +1,80 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ManyToOne,
-    JoinColumn,
-    getRepository
-  } from "typeorm";
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  getRepository
+} from "typeorm";
 
-  import {User} from './User';
-  import {Advertisement} from './Advertisement';
-  var randomize = require('randomatic');
-  
-  enum PaymentMethods {
-    Apple = 'apple',
-    Google = 'google',
-  }
+import { User } from './User';
+import { Advertisement } from './Advertisement';
+var randomize = require('randomatic');
 
-  @Entity('campaigns')
-  // @Unique(["username"])
-  export class Campaign {
-    @PrimaryGeneratedColumn()
-    id: number;
+enum PaymentMethods {
+  Apple = 'apple',
+  Google = 'google',
+}
 
-    @ManyToOne(type => User)
-    @JoinColumn({name:'user_id'})
-    user: User;
+@Entity('campaigns')
+// @Unique(["username"])
+export class Campaign {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @ManyToOne(type => Advertisement)
-    @JoinColumn({name:'advertisement_id'})
-    advertisement: Advertisement;
+  @ManyToOne(type => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
-    @Column()
-    title: string;
-  
-    @Column({length: 511})
-    destination_url: string;
+  @ManyToOne(type => Advertisement)
+  @JoinColumn({ name: 'advertisement_id' })
+  advertisement: Advertisement;
 
-    @Column({length:6})
-    internal_url: string;
+  @Column()
+  title: string;
 
-    @Column({default: 0})
-    views: number;
+  @Column({ length: 511 })
+  destination_url: string;
 
-    @Column()
-    meta_title: string;
+  @Column({ length: 6 })
+  internal_url: string;
 
-    @Column('text')
-    meta_description: string;
+  @Column({ default: 0 })
+  views: number;
 
-    @Column()
-    meta_image: string;
+  @Column({ nullable: true })
+  meta_title: string;
 
-    @Column()
-    @CreateDateColumn()
-    created_at: Date;
-  
-    @Column()
-    @UpdateDateColumn()
-    updated_at: Date;
-    
-    @Column({ name: 'deleted_at', default: null, nullable:true })
-    deleted_at: Date;
-    
+  @Column({ type: 'text', nullable: true })
+  meta_description: string;
 
-    async getInternalID(){
-      const internal_url = randomize('Aa0', 6);
-      var campaign = null
-      do{
-        campaign = await getRepository(Campaign).findOne({
-          where:{internal_url:internal_url}
-        });
-      }
-      while (campaign);
-      return internal_url;
+  @Column({ nullable: true })
+  meta_image: string;
+
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column({ name: 'deleted_at', default: null, nullable: true })
+  deleted_at: Date;
+
+
+  async getInternalID() {
+    const internal_url = randomize('Aa0', 6);
+    var campaign = null
+    do {
+      campaign = await getRepository(Campaign).findOne({
+        where: { internal_url: internal_url }
+      });
     }
-  
+    while (campaign);
+    return internal_url;
   }
-  
+
+}
